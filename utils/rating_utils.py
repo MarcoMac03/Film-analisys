93a0d70bef4669bfd67504d80888c4ce1aeadafa
Marco <marconar2003@gmail.com>
2025-08-12 18:55:38 +0200

    Continuing to check and clean data

---
+++ utils/rating_utils.py
@@ -0,0 +1,22 @@
+def classifica_rating(rating):
+    vietati_18 = [
+        'R', 'NC-17', 'VM18', '18', '18A', 'R18', 'R-18', '18PL', 'C18', 'M/18',
+        'MA15+', 'R18+', 'R15+', '18SG', '18TC', 'III', 'IM-18', '18X', 'A-18',
+        'R21', 'R-15', 'R15', 'R-15+', 'MA+', 'IM18', 'SAM18', '18PA', '18SX',
+        'M21', 'M-18', 'PG18', 'NC18', 'NC-16', 'NC-15', 'NC16', 'R16', 'R-16',
+        'R13+', 'R-13', 'PG-17', 'PG-16', 'PG15', 'PG-15', 'PG-14', 'PG13',
+        'PG-13', '15', '16', '17', '14A', '15A', '16E', '16LS', '16LV', '16SL',
+        '16LPV', 'VM14', 'M/14', 'M14', 'M16', 'M/16', 'M/16Q', 'IM16'
+    ]
+
+    per_tutti = [
+        'G', 'ATP', 'U', 'ALL', 'TV-G', 'FSK0', 'FSK-0', 'TV-Y7', 'TV-Y', 'SU',
+        '0', 'AL', 'AG', 'GA', 'KT/EA', 'EA', 'K+', 'K', 'K7', 'K-7', 'K-6-4'
+    ]
+
+    if rating in vietati_18:
+        return 'Minori di 18'
+    elif rating in per_tutti:
+        return 'Per tutti'
+    else:
+        return 'Altro'
